3403894bad15f5e0a6a3fdbb6aacd06c88f2ff0d
@shr_creation <shrhossain786@gmail.com>
2023-07-14 20:22:45 +0600

    add firebase

---
+++ src/utils/DummyData.js
@@ -0,0 +1,18 @@
+// import firestore from '@react-native-firebase/firestore';
+// const [data, setData] = useState(null);
+// useEffect(() => {
+//   getData();
+// }, []);
+
+// const getData = async () => {
+//   try {
+//     const tempData = await firestore()
+//       .collection('octo-downloader-app')
+//       .doc('zapoMtm0iVE2SkV8NmrE')
+//       .get();
+
+//     console.log(tempData?._data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
