915bf2e565d565cf9b907476df21b4bcdab70cc0
MD. Shahadot Hossain Rahat <shrhossain786@gmail.com>
2023-08-02 19:22:07 +0600

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,7 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react'],
+  root: true,
+  extends: '@react-native-community',
   rules: {
-    'react/prop-types': 'off',
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
   },
 };
